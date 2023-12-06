6997afc3061ce5a1f480e7f3d6b34a407d368283
agihalputra <146146734+agihalputra@users.noreply.github.com>
2023-12-06 11:40:22 +0700

    Add files via upload

---
+++ Task 11/aritmatika.py
@@ -0,0 +1,53 @@
+def tambah(bilangan1, bilangan2):
+    hasil = bilangan1 + bilangan2
+    print("Hasilnya adalah: ", hasil)
+
+def kurang(bilangan1, bilangan2):
+    hasil = bilangan1 - bilangan2
+    print("Hasilnya adalah: ", hasil)
+
+def kali(bilangan1, bilangan2):
+    hasil = bilangan1 * bilangan2
+    print("Hasilnya adalah: ", hasil)
+
+def bagi(bilangan1, bilangan2):
+    hasil = bilangan1 / bilangan2
+    print("Hasilnya adalah: ", hasil)
+
+def akar_kuadrat(bilangan1, bilangan2):
+    hasil = bilangan1 ** 0,5, bilangan2
+    print("Hasilnya adalah: ", hasil)
+
+def modulus(bilangan1, bilangan2):
+    hasil = bilangan1 % bilangan2
+    print("Hasilnya adalah: ", hasil)
+
+def pangkat(bilangan1, bilangan2):
+    hasil = bilangan1 ** bilangan2
+    print("Hasilnya adalah: ", hasil)
+
+def akar_pangkat_tiga(bilangan1):
+    hasil = bilangan1 ** (1/3)
+    print("Hasilnya adalah: ", hasil)
+
+def akar_pangkat_empat(bilangan1):
+    hasil = bilangan1 ** (1/4)
+    print("Hasilnya adalah: ", hasil)
+
+def akar_pangkat_lima(bilangan1):
+    hasil = bilangan1 ** (1/5)
+    print("Hasilnya adalah: ", hasil)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Task 11/index.py
@@ -0,0 +1,10 @@
+from bangun_datar import *
+
+persegi(20)
+
+segitiga_sama_sisi(5,4)
+
+from aritmatika import *
+
+akar_pangkat_tiga(4)
+
